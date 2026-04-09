fcefb0da4c91de920a1c3be9f7d793ae53fedd13
Bryleigh Ballesteros <bab3@hawaii.edu>
2026-04-09 07:58:41 -1000

    Problem Sets completed

--- HackwichTwelve_IntroMapKit/ViewController.swift
+++ HackwichTwelve_IntroMapKit/ViewController.swift
@@ -14,12 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     //initial location
     let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
-    let regionRadius: CLLocationDistance = 1000
+    let regionRadius: CLLocationDistance = 10000
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         centerMapOnLocation(location: initialLocation)
+        
+        //add our first annotation to the map
+        let restaurantOne = restaurantObject(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
+        
+        //add our second annotation to the map (problem sets)
+        let restaurantTwo = restaurantObject(title: "Italian", type: "Assaggio", coordinate: CLLocationCoordinate2D(latitude: 21.332778, longitude: -158.082889))
+        mapView.addAnnotation(restaurantTwo)
     }
 
     func centerMapOnLocation(location: CLLocation)
